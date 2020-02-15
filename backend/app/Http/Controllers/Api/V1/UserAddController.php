5a0a85660dc14635d45e3cf2431af00ae0c905e1
v.badyaev <vvadzimko@gmail.com>
2020-02-15 23:12:08 +0300

    user create added

--- backend/app/Http/Controllers/Api/V1/UserAddController.php
+++ backend/app/Http/Controllers/Api/V1/UserAddController.php
@@ -6,34 +6,34 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiRequest;
 use App\Models\User;
-use Illuminate\Support\Facades\Redis;
 
 class UserAddController extends Controller {
 
     public function index(ApiRequest $request) {
-        $user_id      = (int)$request->get('id');
         $user_name    = (string)$request->get('name');
         $user_surname = (string)$request->get('surname');
 
-        if (!$user_id || !$user_surname || !$user_name) {
+        if (!$user_surname || !$user_name) {
             return response()->json([
                 'response' => 'bad',
             ]);
         }
 
+        $user_floor   = (int)$request->get('floor');
         $user_cabinet = (int)$request->get('cabinet');
         $user_level   = (int)$request->get('level');
 
-        $save_result = User::add($user_id, $user_name, $user_surname, $user_cabinet, $user_level);
-        if (!$save_result) {
+        $new_user_id = User::create($user_name, $user_surname, $user_floor, $user_cabinet, $user_level);
+        if (!$new_user_id) {
             return response()->json([
                 'response' => 'bad',
-                'desc' => 'save result',
+                'desc'     => 'save result failed',
             ]);
         }
 
         return response()->json([
             'response' => 'ok',
+            'user_id'  => $new_user_id,
         ]);
     }
 }
