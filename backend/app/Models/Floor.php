a19cf8de833e53fcc95bf5b250b01237962f6f89
v.badyaev <vvadzimko@gmail.com>
2020-02-16 00:08:13 +0300

    floors added

--- backend/app/Models/Floor.php
+++ backend/app/Models/Floor.php
@@ -1,34 +1,50 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Redis;
 
-class Floor extends Model
-{
-    use Notifiable;
+class Floor {
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    private static function getKey($floor_id) {
+        return 'floor' . $floor_id;
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-    ];
+    private static function getSetKey() {
+        return 'floors';
+    }
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
+    public static function add($id, $cabinets) {
+        $floor            = [
+            'id'        => $id,
+            'cabinets'  => $cabinets,
+        ];
+        $floor_serialized = json_encode($floor, JSON_UNESCAPED_UNICODE);
 
-    ];
+        $result = (bool)Redis::set(self::getKey($id), $floor_serialized);
+        if ($result) {
+            Redis::sadd(self::getSetKey(), $id);
+        }
+
+        return $result;
+    }
+
+    public static function get($id) {
+        $floor_serialized = Redis::get(self::getKey($id));
+        if (!$floor_serialized) {
+            return null;
+        }
+
+        return json_decode($floor_serialized, true);
+    }
+
+    public static function getAll() {
+        $all_floor_ids = (array)Redis::smembers(self::getSetKey());
+        $floors        = [];
+        foreach ($all_floor_ids as $floor_id) {
+            $floors[] = self::get($floor_id);
+        }
+
+        return $floors;
+    }
 }
