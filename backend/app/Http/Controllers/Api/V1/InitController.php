74ad922dd2c00beeabd54fc5927fadf2bc81f02e
Egor Gornak <egorgornak@gmail.com>
2020-02-16 00:13:49 +0300

    add floor parsing

--- backend/app/Http/Controllers/Api/V1/InitController.php
+++ backend/app/Http/Controllers/Api/V1/InitController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiRequest;
+use App\Models\Floor;
 use App\Models\User;
 use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
 
@@ -26,16 +27,25 @@ class InitController extends Controller
         }
     }
 
-    public function parseFloorFile(String $file_name) {
+    public function parseFloorFile(String $file_name, int $floor_id) {
         $content = file_get_contents($file_name);
         $cabinets = explode("\n", $content);
+
+
+        $cabinets_data = [];
         foreach ($cabinets as $cabinet) {
             $test = explode(";", $cabinet);
             if (sizeof($test) == 4) {
                 list($cabinet_id, $cabinet_name, $point_x, $point_y) = $test;
-                info($test);
+               $cabinets_data[] = [
+                   'id' => $cabinet_id,
+                   'name' => $cabinet_name,
+                   'point_x' => $point_x,
+                   'point_y' => $point_y,
+               ];
             }
         }
+        Floor::add($floor_id, $cabinets_data);
     }
 
     public function parseUser() {
@@ -44,8 +54,8 @@ class InitController extends Controller
     }
 
     public function parseFloors() {
-        self::parseFloorFile(__DIR__ . "/../../../../src/floors/1_floor;id;name;x;y");
-        self::parseFloorFile(__DIR__ . "/../../../../src/floors/3_floor;id;name;x;y");
+        self::parseFloorFile(__DIR__ . "/../../../../src/floors/1_floor;id;name;x;y", 1);
+        self::parseFloorFile(__DIR__ . "/../../../../src/floors/3_floor;id;name;x;y", 3);
     }
 
     public function index(ApiRequest $request)
