99b6d2d944cf3120373580af7369acbc0f65b8ba
Nikolai Zakharevich <nikolayzakharevich@gmail.com>
2020-02-16 01:46:55 +0300

    linked to backend

---
+++ frontend/my-app/src/api/floor.js
@@ -0,0 +1,3 @@
+import Backend from "../tools/Backend";
+
+export const getFloor = ({}) => Backend.request('v1/floor', {}, );
---
+++ frontend/my-app/src/config.js
@@ -0,0 +1,18 @@
+const configs = {
+    production: {
+        "app_id": 7109110,
+        "redirect_uri": "http://5dea4276.ngrok.io/app/",
+        "basepath": "/",
+        "api_basepath": "http://5dea4276.ngrok.io/api/"
+    },
+    local: {
+        "app_id": 7109125,
+        "redirect_uri": "https://127.0.0.1:3000/",
+        "basepath": "/",
+        "api_basepath": "http://127.0.0.1:8000/api/"
+    }
+};
+
+const returnConfig = configs[process.env.REACT_APP_CONFIG] || configs.production;
+
+export default returnConfig;
