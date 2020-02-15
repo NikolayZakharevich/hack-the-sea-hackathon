5d8d98ded5951790fa54edb910642591268e1511
Egor Gornak <egorgornak@gmail.com>
2020-02-16 00:08:42 +0300

    move files

--- backend/app/Http/Controllers/Api/V1/InitController.php
+++ backend/app/Http/Controllers/Api/V1/InitController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiRequest;
+use App\Models\User;
 use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
 
 class InitController extends Controller
@@ -20,19 +21,37 @@ class InitController extends Controller
             if (sizeof($test) == 3) {
                 list($cabinet_id, $surname, $name) = $test;
                 $floor = substr($cabinet_id, 0, 1);
-                info($cabinet_id." ".$surname." ".$name." ".$level. " ".$floor);
+                User::create($name, $surname, $floor, $cabinet_id, $level);
+            }
+        }
+    }
+
+    public function parseFloorFile(String $file_name) {
+        $content = file_get_contents($file_name);
+        $cabinets = explode("\n", $content);
+        foreach ($cabinets as $cabinet) {
+            $test = explode(";", $cabinet);
+            if (sizeof($test) == 4) {
+                list($cabinet_id, $cabinet_name, $point_x, $point_y) = $test;
+                info($test);
             }
         }
     }
 
     public function parseUser() {
-        self::parseUserFile(__DIR__ . "/1_level;cab;surname;name.txt", 1);
-        self::parseUserFile(__DIR__ . "/2_level;cab;surname;name.txt", 2);
+        self::parseUserFile(__DIR__ . "/../../../../src/users/1_level;cab;surname;name.txt", 1);
+        self::parseUserFile(__DIR__ . "/../../../../src/users/2_level;cab;surname;name.txt", 2);
+    }
+
+    public function parseFloors() {
+        self::parseFloorFile(__DIR__ . "/../../../../src/floors/1_floor;id;name;x;y");
+        self::parseFloorFile(__DIR__ . "/../../../../src/floors/3_floor;id;name;x;y");
     }
 
     public function index(ApiRequest $request)
     {
         self::parseUser();
+        self::parseFloors();
     }
 }
 
