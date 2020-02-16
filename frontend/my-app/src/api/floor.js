14dbb26d608fad84b49b50724ca7b4f704f1f080
Nikita Savinov <savinov_nikitka@mail.ru>
2020-02-16 04:27:28 +0300

    update

--- frontend/my-app/src/api/floor.js
+++ frontend/my-app/src/api/floor.js
@@ -1,3 +1,4 @@
 import Backend from "../tools/Backend";
 
 export const getFloor = id => Backend.request('v1/floor/' + id, {}, );
+export const filterResults = (id, filters) => Backend.request('v1/floor' + id, {filters});
---
+++ frontend/my-app/src/api/search.js
@@ -0,0 +1,3 @@
+import Backend from "../tools/Backend";
+
+export const search = query => Backend.request('v1/search', {query},);
