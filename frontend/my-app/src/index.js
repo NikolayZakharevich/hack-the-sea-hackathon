6a085ac6da4072a29a0982ebb4cf70446293e4b7
Nikolai Zakharevich <nikolayzakharevich@gmail.com>
2020-02-16 07:43:18 +0300

    replaced routing by state

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,16 +1,12 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import App from './App'
-import { RouterProvider } from 'react-router5'
-import createRouter from './create-router'
-import "typeface-comfortaa";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const router = createRouter()
+ReactDOM.render(<App />, document.getElementById('root'));
 
-router.start(() => {
-    ReactDOM.render((
-        <RouterProvider router={router}>
-            <App />
-        </RouterProvider>
-    ), document.getElementById('root'))
-});
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- frontend/my-app/src/routes.js
+++
@@ -1,9 +0,0 @@
-export const PAGE_CABINET = 'cabinet'
-export const PAGE_FLOOR = 'floor'
-export const PAGE_BUILDING = 'building'
-
-export default [
-    { name: PAGE_BUILDING, path: '/index' },
-    { name: PAGE_FLOOR, path: '/floor/:id' },
-    { name: PAGE_CABINET, path: '/cabinet/:id' },
-]
